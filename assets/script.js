f85c90b52141b9466c80d5f94e5e97d588686fcf
Young Jang <jyoungjoon@gmail.com>
2023-03-07 22:55:10 -0800

    testing again

--- assets/script.js
+++ assets/script.js
@@ -17,16 +17,10 @@ function selectTopics(){
   } else if (randomTopic === 'JavaScript') {
     console.log("Let's study JavaScript!");
   } else {
-    console.log('Please try again!');
+    console.log('I will never be displayed! SadFace');
   }
 }
 console.log('Here are the topics we learned through Prework:');
 listTopics();
 console.log('Which topic should we study first?');
 selectTopics();
-
-console.log('Here are the topics we learned through Prework:');
-listTopics();
-console.log('Which topic should we study first?');
-selectTopics();
-
